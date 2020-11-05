da9eeed3892603ebd769e5e9326b45d72870cd6c
morgan pierson <morganpierson@gmail.com>
2020-11-04 17:31:53 -0800

    side bar complete

--- js/index.js
+++ js/index.js
@@ -1,11 +1,14 @@
 const fetchData = () => {
     axios.get("https://spreadsheets.google.com/feeds/list/1H5S6Vc-gCOCKLvQmfjfJmG2THtDb5Z_LQGaZJpWZQ4c/1/public/values?alt=json")
         .then(res => {
-            const data = res.data.feed.entry.map(song => {
-                return song.content
-            })
-            console.log("SONGS ", data)
-            document.querySelector(".main").innerHTML = `<div>${data[0]["$t"]}</div>`
+            console.log("SONGS ", res.data.feed.entry)
+            let data = res.data.feed.entry
+            // using for loop over forEach to ensure we limit to first 100 entries
+            for(let i = 0; i < 100; i++) {
+                let li = document.createElement('li');
+                li.innerHTML = data[i].content["$t"]
+                document.querySelector(".main__content--song-list").appendChild(li)
+            } 
         })    
 }
 
