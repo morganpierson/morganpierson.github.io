c5d95e2ab41f58ad4aaf46d43746647e09d70b45
morgan pierson <morganpierson@gmail.com>
2020-11-07 17:32:36 -0800

    main content begin formatting

--- js/index.js
+++ js/index.js
@@ -1,13 +1,53 @@
 const fetchData = () => {
+    
     axios.get("https://spreadsheets.google.com/feeds/list/1H5S6Vc-gCOCKLvQmfjfJmG2THtDb5Z_LQGaZJpWZQ4c/1/public/values?alt=json")
         .then(res => {
-            console.log("SONGS ", res.data.feed.entry)
-            let data = res.data.feed.entry
-            // using for loop over forEach to ensure we limit to first 100 entries
-            for(let i = 0; i < 100; i++) {
-                let li = document.createElement('li');
-                li.innerHTML = data[i].content["$t"]
-                document.querySelector(".main__content--song-list").appendChild(li)
+            let data = res.data.feed.entry.splice(0, 100)
+            let timeDiff;
+            let seconds;
+            let minutes;
+            let songData;
+            
+            for(let i = 0; i < data.length; i++) {
+                console.log("DATA ", data[i])
+                songData = {}
+                timeDiff = data[i].gsx$durationms["$t"]
+                seconds = timeDiff / 1000
+                minutes = parseInt(seconds / 60)
+                seconds = seconds % 60
+                seconds = Math.floor(seconds)
+                songData.artist = data[i].gsx$artist["$t"]
+                songData.songTitle = data[i].gsx$songtitle["$t"]
+                songData.album = "Hey Pex! Hire Me?"
+                
+                songData.dateAdded = `Nov 7, 2020`
+                songData.duration = `${minutes}:${seconds < 10 ? "0" + seconds : seconds}`
+                console.log("SONG DATA ", songData)
+                let tr = document.createElement('tr');
+                tr.style.color = "white"
+                tr.className = "song-content"
+                tr.innerHTML = `
+                <td>
+                    ${i + 1}
+                </td>
+                <td>
+                    <div class="song-title-artist">
+                        <div>${songData.songTitle}</div>
+                        <div>${songData.artist}</div>
+                    </div>
+                </td>
+                <td>
+                    ${songData.album}
+                </td>
+                <td>
+                    ${songData.dateAdded}
+                </td>
+                <td>
+                    ${songData.duration}
+                </td>
+                `
+                // li.innerHTML = `<div class="track-num-artist"><div>${i + 1}</div> <div class="song-title-artist"><p>${songData.songTitle}</p> <p>${songData.artist}</p></div></div> <div>${songData.album}</div> <div>${songData.dateAdded}</div> <div>${songData.duration}</div>`
+                document.querySelector(".song-data-content").appendChild(tr)
             } 
         })    
 }
